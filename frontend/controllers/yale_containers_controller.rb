b7d0a3ff895300f8c31db5cb30d443a4e2c26a4e
Payten Giles <payten.giles@gmail.com>
2014-12-03 17:17:44 +1100

    Add ability to create yale containers from the linker in the Accession form

--- frontend/controllers/yale_containers_controller.rb
+++ frontend/controllers/yale_containers_controller.rb
@@ -14,6 +14,8 @@ class YaleContainersController < ApplicationController
     @yale_container_hierarchy["yale_container_1"] = JSONModel(:yale_container).new._always_valid!
     @yale_container_hierarchy["yale_container_2"] = JSONModel(:yale_container).new._always_valid!
     @yale_container_hierarchy["yale_container_3"] = JSONModel(:yale_container).new._always_valid!
+
+    render_aspace_partial :partial => "yale_containers/new" if inline?
   end
 
 
@@ -25,12 +27,24 @@ class YaleContainersController < ApplicationController
 
       if @yale_container_hierarchy._exceptions.blank?
         begin
-          @yale_container_hierarchy.save
+          result = @yale_container_hierarchy.save({}, true)
+
+          if inline?
+            if result["uris"].length > 0
+              yale_container_uri = result["uris"].last
+            else
+              # if we didn't create a container then we just take the third container
+              yale_container_uri = @yale_container_hierarchy["yale_container_3"]
+            end
+
+            yale_container_id = JSONModel(:yale_container).id_for(yale_container_uri)
+            render :json => JSONModel(:yale_container).find(yale_container_id)
+          else
+            redirect_to(:controller => :yale_containers, :action => :index)
+          end
         rescue JSONModel::ValidationException => ex
           handle_error
         end
-
-        redirect_to(:controller => :yale_containers, :action => :index)
       else
         handle_error
       end
@@ -40,19 +54,6 @@ class YaleContainersController < ApplicationController
 
   def show
     @yale_container = JSONModel(:yale_container).find(params[:id])
-  end
-
-
-  def handle_error
-    @exceptions = @yale_container_hierarchy._exceptions
-
-    @yale_container_hierarchy["ref"] = params["yale_container_hierarchy"]["ref"]
-
-    if params["yale_container_hierarchy"] && params["yale_container_hierarchy"]["_resolved"]
-      @yale_container_hierarchy["_resolved"] = ASUtils.json_parse(params["yale_container_hierarchy"]["_resolved"])
-    end
-
-    render :action => :new
   end
 
 
@@ -68,6 +69,25 @@ class YaleContainersController < ApplicationController
   end
 
   def batch_delete
+  end
+
+
+  private
+
+  def handle_error
+    @exceptions = @yale_container_hierarchy._exceptions
+
+    @yale_container_hierarchy["ref"] = params["yale_container_hierarchy"]["ref"]
+
+    if params["yale_container_hierarchy"] && params["yale_container_hierarchy"]["_resolved"]
+      @yale_container_hierarchy["_resolved"] = ASUtils.json_parse(params["yale_container_hierarchy"]["_resolved"])
+    end
+
+    if inline?
+      render_aspace_partial :partial => "yale_containers/new"
+    else
+      render :action => :new
+    end
   end
 
 end
