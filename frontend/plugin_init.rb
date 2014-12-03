cb36369760e55548684cb18978b695353d76d546
Mark Triggs <mark@dishevelled.net>
2014-12-03 16:00:20 +1100

    Resolve yale containers when fetching records.

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -3,4 +3,17 @@ ArchivesSpace::Application.config.paths['config/routes'].concat(my_routes)
 
 Rails.application.config.after_initialize do
   require_relative "../yale_container_init"
+
+  ApplicationController.class_eval do
+
+    alias_method :find_opts_pre_yale_container, :find_opts
+
+    def find_opts
+      orig = find_opts_pre_yale_container
+      orig['resolve[]'] = orig['resolve[]'] + ['yale_container']
+      orig
+    end
+
+  end
+
 end
