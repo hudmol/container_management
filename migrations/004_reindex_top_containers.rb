15ce8315cba5180c354a7e7cf505f6ca914dccf9
Mark Triggs <mark@dishevelled.net>
2015-01-14 11:59:42 +1100

    Add a migration to reindex top containers

---
+++ migrations/004_reindex_top_containers.rb
@@ -0,0 +1,13 @@
+require 'db/migrations/utils'
+
+Sequel.migration do
+
+  up do
+    self[:top_container].update(:system_mtime => Time.now)
+    self[:container_profile].update(:system_mtime => Time.now)
+  end
+
+  down do
+  end
+
+end
