f2744a7191cf613b0a83b2345fa425c6eb6fe831
jambun <jambun@gmail.com>
2015-01-20 13:11:56 -0500

    fix db migration bug under mysql

--- migrations/003_drop_top_container_type.rb
+++ migrations/003_drop_top_container_type.rb
@@ -4,6 +4,9 @@ Sequel.migration do
 
   up do
     alter_table(:top_container) do
+      if $db_type == :mysql
+        drop_constraint(:top_container_ibfk_1, :type => :foreign_key)
+      end
       drop_column(:type_id)
     end
   end
