6315c810711c847970815b512214d4cf0d6e79a9
Payten Giles <payten.giles@gmail.com>
2015-02-09 15:24:16 +1100

    Introduce new endpoint to handle custom linker browse behaviour for the top container linker

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -2,6 +2,7 @@ ArchivesSpace::Application.routes.draw do
   match('/plugins/top_containers/search/typeahead' => 'top_containers#typeahead', :via => [:get])
   match('/plugins/top_containers/bulk_operations/search' => 'top_containers#bulk_operations', :via => [:get])
   match('/plugins/top_containers/bulk_operations/search' => 'top_containers#bulk_operation_search', :via => [:post])
+  match('/plugins/top_containers/bulk_operations/browse' => 'top_containers#bulk_operations_browse', :via => [:get])
   match('/plugins/top_containers/batch_delete' => 'top_containers#batch_delete', :via => [:post])
   match('/plugins/top_containers/:id' => 'top_containers#update', :via => [:post])
 
