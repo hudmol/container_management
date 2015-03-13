997404cac8f85bca6bdbd882a505f1e87adb89d1
jambun <jambun@gmail.com>
2015-03-13 13:20:04 -0400

    added support for setting the units, and first pass at populating an extent record

--- backend/controllers/extent_calculator.rb
+++ backend/controllers/extent_calculator.rb
@@ -2,14 +2,17 @@ class ArchivesSpaceService < Sinatra::Base
 
   Endpoint.get('/extent_calculator')
   .description("Calculate the extent of an archival object tree")
-  .params(["record_uri", String, "The uri of the object"])
+  .params(["record_uri", String, "The uri of the object"],
+          ["unit", String, "The unit of measurement to use"])
   .permissions([])
   .returns([200, "Calculation results"]) \
   do
     parsed = JSONModel.parse_reference(params[:record_uri])
     RequestContext.open(:repo_id => JSONModel(:repository).id_for(parsed[:repository])) do
       obj = Kernel.const_get(parsed[:type].to_s.camelize)[parsed[:id]]
-      json_response(ExtentCalculator.new(obj).to_hash)
+      ext_cal = ExtentCalculator.new(obj)
+      ext_cal.units = params[:unit].intern
+      json_response(ext_cal.to_hash)
     end
   end
 
--- frontend/controllers/extent_calculator_controller.rb
+++ frontend/controllers/extent_calculator_controller.rb
@@ -6,7 +6,12 @@ class ExtentCalculatorController < ApplicationController
 
   def index
     if params['record_uri']
-      @results = JSONModel::HTTP::get_json("/extent_calculator", {'record_uri' => params['record_uri']})
+      @results = JSONModel::HTTP::get_json("/extent_calculator", {'record_uri' => params['record_uri'], 'unit' => 'feet'})
+      @extent = JSONModel(:extent).new
+      @extent.number = @results['total_extent']
+      @extent.extent_type = 'linear_feet'
+      container_cardinality = @results['container_count'] == 1 ? 'container' : 'containers'
+      @extent.container_summary = "(#{@results['container_count']} #{container_cardinality})"
     else
       @results = "no object"
     end
